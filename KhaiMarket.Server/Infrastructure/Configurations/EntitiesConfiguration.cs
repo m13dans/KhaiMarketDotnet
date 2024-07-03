168fd7587721ef2e0d6a983e6ab362b4cac06c44
Danur Arya <danuraryapratama@gmail.com>
2024-07-03 11:31:10 +0700

    Remove address from appuser configuration

--- KhaiMarket.Server/Infrastructure/Configurations/EntitiesConfiguration.cs
+++ KhaiMarket.Server/Infrastructure/Configurations/EntitiesConfiguration.cs
@@ -13,18 +13,17 @@ public class AppUserConfig : IEntityTypeConfiguration<AppUser>
         builder.Property(x => x.GivenName).HasMaxLength(256).IsUnicode(false);
         builder.Property(x => x.UserName).HasMaxLength(256).IsUnicode(false);
         builder.Property(x => x.Email).HasMaxLength(256).IsUnicode(false);
-        builder.HasMany(x => x.Addresses).WithMany();
     }
 }
 
-public class AddressConfig : IEntityTypeConfiguration<Address>
-{
-    public void Configure(EntityTypeBuilder<Address> builder)
-    {
-        builder.Property(x => x.AddressDetail).IsUnicode(false);
-        builder.Property(x => x.District).IsUnicode(false);
-        builder.Property(x => x.City).IsUnicode(false);
-        builder.Property(x => x.Province).IsUnicode(false);
-        builder.Property(x => x.ZipCode).IsUnicode(false);
-    }
-}
+// public class AddressConfig : IEntityTypeConfiguration<Address>
+// {
+//     public void Configure(EntityTypeBuilder<Address> builder)
+//     {
+//         builder.Property(x => x.AddressDetail).IsUnicode(false);
+//         builder.Property(x => x.District).IsUnicode(false);
+//         builder.Property(x => x.City).IsUnicode(false);
+//         builder.Property(x => x.Province).IsUnicode(false);
+//         builder.Property(x => x.ZipCode).IsUnicode(false);
+//     }
+// }
