d70e106623cc39a075fc8fcb7ad43cf3ce1fab70
Danur Arya <danuraryapratama@gmail.com>
2024-07-09 11:09:41 +0700

    add required attribute to some product properties so it appears as required in openapi

--- KhaiMarket.Server/Entities/Product.cs
+++ KhaiMarket.Server/Entities/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace KhaiMarket.Server.Entities;
 public class Product
 {
     public int Id { get; set; }
+    [Required]
     public required string Name { get; set; }
+    [Required]
     public required string Description { get; set; }
     public int Stock { get; set; }
     public decimal Price { get; set; }
